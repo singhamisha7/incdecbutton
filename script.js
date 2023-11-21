73396c4a1f7a472aa42e74a373283ecb664ac68b
singhamisha7 <singhamisha70700@gmail.com>
2023-11-21 20:47:51 +0530

    task added

---
+++ script.js
@@ -0,0 +1,15 @@
+var result = 0;
+function count(x) {
+    console.log('clicked');
+    if(x == '-'){
+        result -= 1;
+        document.getElementById('result').innerHTML = result;
+        if (result<0) {
+            document.getElementById('result').innerHTML = 0;
+        }
+    }
+    else{
+        result += 1;
+        document.getElementById('result').innerHTML = result;
+    }
+}
